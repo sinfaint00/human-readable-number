b0c79ff8b8df9a8ba176465c2362aa25adf04abe
sinfaint00 <122840916+sinfaint00@users.noreply.github.com>
2023-08-15 19:42:01 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,29 +1,36 @@
 function toReadable(number) {
-    const ones = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-    const teens = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-    const tens = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    const singleDigits = [
+        "zero", "one", "two", "three", "four",
+        "five", "six", "seven", "eight", "nine"
+    ];
 
-    if (number >= 0 && number <= 9) {
-        return ones[number];
-    } else if (number >= 11 && number <= 19) {
-        return teens[number - 11];
-    } else if (number >= 10 && number <= 99) {
-        if (number % 10 === 0) {
-            return tens[number / 10 - 1];
+    const teens = [
+        "ten", "eleven", "twelve", "thirteen", "fourteen",
+        "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"
+    ];
+
+    const tens = [
+        "", "", "twenty", "thirty", "forty",
+        "fifty", "sixty", "seventy", "eighty", "ninety"
+    ];
+
+    if (number < 10) {
+        return singleDigits[number];
+    } else if (number >= 10 && number < 20) {
+        return teens[number - 10];
+    } else if (number >= 20 && number < 100) {
+        const tensDigit = Math.floor(number / 10);
+        const onesDigit = number % 10;
+        return onesDigit === 0 ? tens[tensDigit] : `${tens[tensDigit]} ${singleDigits[onesDigit]}`;
+    } else {
+        const hundredsDigit = Math.floor(number / 100);
+        const remainingNumber = number % 100;
+        if (remainingNumber === 0) {
+            return `${singleDigits[hundredsDigit]} hundred`;
         } else {
-            return tens[Math.floor(number / 10 - 1)] + ' ' + ones[number % 10];
-        }
-    } else if (number >= 100 && number <= 999) {
-        if (number % 100 === 0) {
-            return ones[Math.floor(number / 100)] + ' hundred';
-        } else if (number % 100 <= 9) {
-            return ones[Math.floor(number / 100)] + ' hundred ' + ones[number % 100];
-        } else if (number % 100 >= 11 && number % 100 <= 19) {
-            return ones[Math.floor(number / 100)] + ' hundred ' + teens[number % 100 - 11];
-        } else if (number % 10 === 0) {
-            return ones[Math.floor(number / 100)] + ' hundred ' + tens[Math.floor((number % 100) / 10) - 1];
-        } else {
-            return ones[Math.floor(number / 100)] + ' hundred ' + tens[Math.floor((number % 100) / 10) - 1] + ' ' + ones[number % 10];
+            return `${singleDigits[hundredsDigit]} hundred ${toReadable(remainingNumber)}`;
         }
     }
 }
+
+module.exports = toReadable;
